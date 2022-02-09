15e8701ad72879b22b29516e5cd80465189d658d
unknown <tangmufeng424@gmail.com>
2022-02-09 18:46:05 +0000

    adding pytorch implementations

--- recurrent_pcns.py
+++ recurrent_pcns.py
@@ -9,64 +9,85 @@ from get_data import get_cifar10, get_fashionMNIST
 
 device = "cpu"
 
-class RecPCN(object):
+class RecPCN(nn.Module):
     def __init__(self, dim):
+        super().__init__()
         self.dim = dim
-
         # initialize parameters
         bound = 1. / np.sqrt(dim)
-        self.Wr = torch.zeros((dim, dim)).fill_diagonal_(0).to(device)
+        self.Wr = nn.Parameter(torch.zeros((dim, dim)))
+
+    def forward(self, X):
+        return torch.matmul(X, self.Wr.t())
         
     def learning(self, X):
-        preds = torch.matmul(X, self.Wr.t())
+        preds = self.forward(X)
         errs = X - preds
         grad_Wr = torch.matmul(errs.T, X).fill_diagonal_(0)
 
-        return grad_Wr
+        self.Wr.grad = -grad_Wr
 
     def inference(self, X_c):
-        errs_X = X_c - torch.matmul(X_c, self.Wr.t())
+        errs_X = X_c - self.forward(X_c)
         delta_X = -errs_X
 
         return delta_X
 
 
 learning_iters = 100
-learning_lr = 0.001
+# learning_lr = 0.0015
 inference_iters = 100
-inference_lr = 0.1
-sample_size = 50
-batch_size = 1
+inference_lr = 1.0
+# sample_size = 10
+batch_size = 10
 cover_size = 12
 image_size = 28
 
-pcn = RecPCN(dim=image_size**2)
-X, X_c, update_mask = get_fashionMNIST('./data', sample_size=sample_size, 
-                                  batch_size=batch_size, 
-                                  seed=10, 
-                                  cover_size=cover_size, 
-                                  device=device)
+mses = []
+for learning_lr, sample_size in zip([1e-3, 5e-4, 2e-4], [100,200,400]):
+    X, X_c, update_mask = get_fashionMNIST('./data', 
+                                        sample_size=sample_size, 
+                                        batch_size=batch_size, 
+                                        seed=10, 
+                                        cover_size=cover_size, 
+                                        device=device)
+    pcn = RecPCN(dim=image_size**2).to(device)
+    optimizer = torch.optim.SGD(pcn.parameters(), lr=learning_lr)
 
-mse = []
-for i in range(learning_iters):
-    for batch_idx in range(0, sample_size, batch_size):
-        data = X[batch_idx:batch_idx+batch_size]
-        grad_Wr = pcn.learning(data)
-        pcn.Wr += learning_lr * grad_Wr
+    for i in range(learning_iters):
+        for batch_idx in range(0, sample_size, batch_size):
+            data = X[batch_idx:batch_idx+batch_size]
+            optimizer.zero_grad()
+            pcn.learning(data)
+            optimizer.step()
 
-    X_recon = X_c.clone()
-    for j in range(inference_iters):
-        delta_X = pcn.inference(X_recon)
-        X_recon += inference_lr * (delta_X * update_mask)
+        X_recon = X_c.clone()
+        for j in range(inference_iters):
+            delta_X = pcn.inference(X_recon)
+            X_recon += inference_lr * (delta_X * update_mask)
 
-    mse.append(torch.mean((X_recon - X)**2))
+        # mse.append(torch.mean((X_recon - X)**2).cpu().detach().numpy())
 
-fig, ax = plt.subplots(5, 3, figsize=(5,8))
-for i in range(5):
-    ax[i, 0].imshow(X[i].cpu().detach().numpy().reshape(image_size, image_size), cmap='gray')
-    ax[i, 0].axis('off')
-    ax[i, 1].imshow(X_c[i].cpu().detach().numpy().reshape(image_size, image_size), cmap='gray')
-    ax[i, 1].axis('off')
-    ax[i, 2].imshow(X_recon[i].cpu().detach().numpy().reshape(image_size, image_size), cmap='gray')
-    ax[i, 2].axis('off')
+    mses.append(torch.mean((X_recon - X)**2, dim=1))
+
+plt.figure()
+for mse, sample_size in zip(mses, [100,200,400]):    
+    plt.hist(mse.cpu().detach().numpy(), density=True, bins='auto', alpha=0.4, edgecolor='k', label=f'{sample_size} memorized')
+plt.legend()
+plt.xlabel('MSE')
+plt.ylabel('Density')
 plt.show()
+
+# plt.figure()
+# plt.plot(mse)
+# plt.show()
+
+# fig, ax = plt.subplots(5, 3, figsize=(5,8))
+# for i in range(5):
+#     ax[i, 0].imshow(X[i].cpu().detach().numpy().reshape(image_size, image_size), cmap='gray')
+#     ax[i, 0].axis('off')
+#     ax[i, 1].imshow(X_c[i].cpu().detach().numpy().reshape(image_size, image_size), cmap='gray')
+#     ax[i, 1].axis('off')
+#     ax[i, 2].imshow(X_recon[i].cpu().detach().numpy().reshape(image_size, image_size), cmap='gray')
+#     ax[i, 2].axis('off')
+# plt.show()
