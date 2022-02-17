f1976a19bad812e57a388f91e6586de2e98a506d
unknown <tangmufeng424@gmail.com>
2022-02-17 14:05:59 +0000

    sampling particular classes

--- get_data.py
+++ get_data.py
@@ -4,6 +4,47 @@ from torchvision.transforms import transforms
 import random
 
 
+def get_mnist(datapath, sample_size, batch_size, seed, cover_size, device, classes=None):
+    # classes: a list of specific class to sample from
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+    ])
+    train = datasets.MNIST(datapath, train=True, transform=transform, download=True)
+    test = datasets.MNIST(datapath, train=False, transform=transform, download=True)
+
+    # subsetting data based on sample size and number of classes
+    idx = sum(train.targets == c for c in classes).bool() if classes else range(len(train))
+    train.targets = train.targets[idx]
+    train.data = train.data[idx]
+    if sample_size != len(train):
+        random.seed(seed)
+        train = torch.utils.data.Subset(train, random.sample(range(len(train)), sample_size))
+
+    train_loader = torch.utils.data.DataLoader(train, batch_size=batch_size, shuffle=False)
+    test_loader = torch.utils.data.DataLoader(test, batch_size=batch_size, shuffle=False)
+
+    X = []
+    for batch_idx, (data, targ) in enumerate(train_loader):
+        X.append(data)
+    X = torch.cat(X, dim=0).squeeze().to(device) # size, 28, 28
+
+    # create the corrupted version
+    size = X.shape
+    mask = torch.ones_like(X).to(device)
+    # mask[:, (size[1]-cover_size)//2:(size[1]+cover_size)//2, (size[2]-cover_size)//2:(size[2]+cover_size)//2] -= 1
+    mask[:, size[1]//2:, :] -= 1
+    update_mask = torch.zeros_like(X).to(device)
+    update_mask[:, size[1]//2:, :] += 1
+    update_mask = update_mask.reshape(-1, 28**2)
+
+    X_c = (X * mask).to(device) # size, 28, 28
+
+    X = X.reshape(-1, 28**2) # size, 784
+    X_c = X_c.reshape(-1, 28**2) # size, 784
+
+    return X, X_c, update_mask
+
+
 def get_cifar10(datapath, sample_size, batch_size, seed, cover_size, device):
     transform = transforms.Compose([
         transforms.Grayscale(num_output_channels=1),
@@ -39,16 +80,21 @@ def get_cifar10(datapath, sample_size, batch_size, seed, cover_size, device):
     return X, X_c, update_mask
 
 
-def get_fashionMNIST(datapath, sample_size, batch_size, seed, cover_size, device):
+def get_fashionMNIST(datapath, sample_size, batch_size, seed, cover_size, device, classes=None):
     transform = transforms.Compose([
         transforms.ToTensor(),
     ])
     train = datasets.FashionMNIST(datapath, train=True, transform=transform, download=True)
     test = datasets.FashionMNIST(datapath, train=False, transform=transform, download=True)
     
+    # subsetting data based on sample size and number of classes
+    idx = sum(train.targets == c for c in classes).bool() if classes else range(len(train))
+    train.targets = train.targets[idx]
+    train.data = train.data[idx]
     if sample_size != len(train):
         random.seed(seed)
         train = torch.utils.data.Subset(train, random.sample(range(len(train)), sample_size))
+
     train_loader = torch.utils.data.DataLoader(train, batch_size=batch_size, shuffle=False)
     test_loader = torch.utils.data.DataLoader(test, batch_size=batch_size, shuffle=False)
 
